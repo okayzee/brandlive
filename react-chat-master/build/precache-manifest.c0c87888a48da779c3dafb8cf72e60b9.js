90882788ce61d002b6c7eb4bf521fa668b9c22bc
kazeem odofin <odofinkayzee01@gmail.com>
2021-10-11 08:45:23 -0500

    READY TO DEPLOY

---
+++ react-chat-master/build/precache-manifest.c0c87888a48da779c3dafb8cf72e60b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258943ed26d574a00b81c390fd9e0dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48fe54ed410e64ace89",
+    "url": "/static/css/main.bebc093d.chunk.css"
+  },
+  {
+    "revision": "7e09485daee77c45a2ac",
+    "url": "/static/js/2.5646d4a3.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.5646d4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48fe54ed410e64ace89",
+    "url": "/static/js/main.29e81850.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
